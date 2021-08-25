c0cc257e38175bfcb5fecfdd50a6fe42c08486ca
webdevsmart1220 <webdevsmart1220@gmail.com>
2021-08-25 22:12:05 +0200

    simpleborrow, ui update

--- src/common/web3/Wallet.js
+++ src/common/web3/Wallet.js
@@ -1,39 +1,43 @@
-import { clearWalletProvider, connectToWallet, web3ModalProvider } from "./web3Modal";
+import {
+  clearWalletProvider,
+  connectToWallet,
+  web3ModalProvider,
+} from "./web3Modal";
 
-export let accountAddress = undefined
-export let web3Modal = undefined
+export let accountAddress = undefined;
+export let web3Modal = undefined;
 
 export async function initWallet() {
-    while(web3Modal === undefined) {
-        console.log("connecting wallet")
-        try {
-            web3Modal = await connectToWallet()
-        } catch (e) {
-            console.log("wallet connect error, reconnecting")
-        }
+  while (web3Modal === undefined) {
+    console.log("connecting wallet");
+    try {
+      web3Modal = await connectToWallet();
+    } catch (e) {
+      console.log("wallet connect error, reconnecting");
     }
-    await updateAccount()
+  }
+  await updateAccount();
 }
 
 async function updateAccount() {
-    const accounts = await web3Modal.eth.getAccounts()
-    updateAccountAddress(accounts)
+  const accounts = await web3Modal.eth.getAccounts();
+  updateAccountAddress(accounts);
 
-    if (web3ModalProvider !== undefined && web3ModalProvider !== null) {
-        web3ModalProvider.on("accountsChanged", (accounts) => {
-            updateAccountAddress(accounts);
-        });
-        web3ModalProvider.on("networkChanged", (id) => {
-            window.location.reload();
-        });
-    }
+  if (web3ModalProvider !== undefined && web3ModalProvider !== null) {
+    web3ModalProvider.on("accountsChanged", (accounts) => {
+      updateAccountAddress(accounts);
+    });
+    web3ModalProvider.on("networkChanged", (id) => {
+      window.location.reload();
+    });
+  }
 }
 
 export function updateAccountAddress(accounts) {
-    if (accounts !== undefined && accounts.length > 0) {
-        accountAddress = accounts[0]
-    } else if (accountAddress !== undefined){
-        clearWalletProvider()
-        accountAddress = undefined
-    }
+  if (accounts !== undefined && accounts.length > 0) {
+    accountAddress = accounts[0];
+  } else if (accountAddress !== undefined) {
+    clearWalletProvider();
+    accountAddress = undefined;
+  }
 }
